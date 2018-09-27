ee8ca727e87699651871b8fc40f453e0aea51cab
Alex Lu <alexlu@0587337739.wireless.umich.net>
2018-09-27 15:30:50 -0400

    Updated the code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { DetailPageModule } from '../pages/detail/detail.module'
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -21,7 +22,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    DetailPageModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
